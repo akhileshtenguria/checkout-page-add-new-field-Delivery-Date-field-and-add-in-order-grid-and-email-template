c4ebb02bd407d35e3f1c6b5a775ffc1fd5aa606e
akhileshtenguria <79194761+akhileshtenguria@users.noreply.github.com>
2022-06-02 14:40:21 +0530

    Update Instruction.php

--- Instruction.php
+++ Instruction.php
@@ -1,1 +1,5 @@
-tests
+1)Menu Marketing > Email Template. -- Create new template for Sales Order Email add our variable then save.
+
+2) Store > Configuration > sales > Sales Emails
+
+Select Our new template 'New Order Confirmation Template' and 'New Order Confirmation Template for Guest'.
