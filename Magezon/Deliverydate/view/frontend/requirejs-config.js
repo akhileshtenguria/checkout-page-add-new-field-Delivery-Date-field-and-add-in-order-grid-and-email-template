e25007a2db9adc057f132edeb58355fd6e74cd18
akhileshtenguria <79194761+akhileshtenguria@users.noreply.github.com>
2022-06-24 17:55:21 +0530

    Update requirejs-config.js

--- Magezon/Deliverydate/view/frontend/requirejs-config.js
+++ Magezon/Deliverydate/view/frontend/requirejs-config.js
@@ -1,9 +1,14 @@
 var config = {
-config: {
-    mixins: {
-        'Magento_Checkout/js/action/place-order': {
-            'Magezon_Deliverydate/js/order/place-order-mixin': true
-        },
+    config: {
+        mixins: {
+            'Magento_Checkout/js/action/place-order': {
+                'Magezon_Deliverydate/js/order/place-order-mixin': true
+            },
+        }
+    },
+    'map': {
+        '*': {
+            'Magento_Checkout/js/view/shipping': 'Magezon_Deliverydate/js/view/shipping'
+        }
     }
-}
 };
